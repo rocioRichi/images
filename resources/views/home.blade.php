2990a037d83547f01a6ae9e52609b8a64325efec
rocioRichi <mailparagit@gmail.com>
2025-04-29 11:46:46 +0200

    Add css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <title>Imágenes</title>
 </head>
+@vite(["resources/css/app.css"])
 <body>
     <h1>Galería de imágenes</h1>
 
-    <ul>
+    <div>
         @foreach ($imagenes as $image)
-            <li>
+
                 <img src="{{ asset('storage/' . $image->image_route) }}" width="200">
-            </li>
+
         @endforeach
-    </ul>
+        </div>
 </body>
 </html>
