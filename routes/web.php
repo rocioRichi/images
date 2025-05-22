e8c3b89ff102370fb37836ca31702ccba15f2e0b
rocioRichi <mailparagit@gmail.com>
2025-05-22 21:01:45 +0200

    Modify routeWeb, adding trycatch

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
-use App\Models\Image;
 use Illuminate\Support\Facades\Route;
+use App\Models\Image;
+use Illuminate\Support\Facades\Log;
 
-Route::get('/', function () {
-    $imagenes = Image::all();
+$callback = function () {
+    try {
+        $imagenes = Image::all();
+    } catch (\Exception $e) {
+        Log::error('Error al obtener imágenes: ' . $e->getMessage());
+        $imagenes = [];
+    }
+
     return view('home', compact('imagenes'));
-});
+};
+
+Route::get('/', $callback);
+Route::get('/images', $callback);
